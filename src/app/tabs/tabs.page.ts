b98a1ff0f6510c869ff94dcb92e8b914d7fb9c20
Vanessa <vanne.jgs@gmail.com>
2019-11-09 14:46:20 -0600

    tabs

--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MenuController } from '@ionic/angular';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-tabs',
@@ -7,6 +9,28 @@ import { Component } from '@angular/core';
 })
 export class TabsPage {
 
-  constructor() {}
+  constructor(private menu: MenuController, public route: Router) {}
 
+  openFirst() {
+    console.log("click OpenFirst");
+    this.menu.enable(true, 'first');
+    this.menu.open('first');
+  }
+
+  openEnd() {
+    console.log("click OpenEnd");
+    this.menu.open('end');
+  }
+
+  openCustom() {
+    console.log("click OpenCustom");
+    this.menu.enable(true, 'custom');
+    this.menu.open('custom');
+  }
+
+  irA(vinculo:string){
+    console.log(vinculo);
+
+    this.route.navigateByUrl(vinculo);
+  }
 }
